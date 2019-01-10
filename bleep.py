873305e21a5132d23ce65fb127618c8c520ea40e
謝宗翰 <pocession@gmail.com>
2019-01-10 09:33:13 +0000

    update

--- bleep.py
+++ bleep.py
@@ -1,5 +1,7 @@
 # bleep
 
+import cs50 as cs50
+import sys as sys
 from cs50 import get_string
 from sys import argv
 
@@ -12,6 +14,27 @@ def main():
         print("Usage: python bleep.py dictionary", end="")
         print()
         exit(1)
+
+    # Get users' entry
+    print("Enter your string: ", end="")
+    string = cs50.get_string()
+    user_input = string.split()
+
+    # Make a banned list
+    banned = [line.strip() for line in open(sys.argv[1], 'r')]
+
+    # Compare
+    print(user_input, end="")
+    print()
+    print(banned, end="")
+    print()
+    a = list(set(user_input) & set(banned))
+
+    print(a, end = "")
+    print()
+
+
+
 
 
 if __name__ == "__main__":
